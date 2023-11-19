e9034cc7cd97c847febe3d7de34aa4d0b6883e80
IviriusMain <106150547+IviriusMain@users.noreply.github.com>
2023-11-20 00:33:40 +0200

    a

--- src/App.svelte
+++ src/App.svelte
@@ -3,8 +3,6 @@
 </script>
 
 <style>
-    @import url("https://unpkg.com/fluent-svelte/theme.css");
-
     /* Some base styles to get things looking right. */
     :global(body) {
         background-color: var(--fds-solid-background-base);
