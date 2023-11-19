e6d1615e8ff0d3887854401c5da8498500eb2ae2
IviriusMain <106150547+IviriusMain@users.noreply.github.com>
2023-11-20 00:36:32 +0200

    a

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,3 @@
 <script>
 	import "fluent-svelte/theme.css";
 </script>
-
-<style>
-    /* Some base styles to get things looking right. */
-    :global(body) {
-        background-color: var(--fds-solid-background-base);
-        color: var(--fds-text-primary);
-    }
-</style>
