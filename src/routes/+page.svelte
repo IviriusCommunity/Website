373c728c87fe5922f622d93a02ed76c5e56c35cb
IviriusMain <106150547+IviriusMain@users.noreply.github.com>
2023-11-20 00:30:26 +0200

    a

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,31 +7,19 @@
 
 <svelte:head>
 	<title>Ivirius</title>
-	<meta name="description" content="Svelte demo app" />
+	<meta name="description" content="Ivirius website" />
 </svelte:head>
 
 <section>
 	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
-
-		to your old<br />SvelteKit app
+		Main Page
 	</h1>
 
 	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<h3>
 		<Button>Click me!</Button>
 		<Checkbox>Check me!</Checkbox>
-	</h3>
+	</h2>
 
-	<Counter />
 </section>
 
 <style>
