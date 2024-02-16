3ae78aaad0c74bb3cd3bf71dc94d46e1988713ff
IviriusMain <106150547+IviriusMain@users.noreply.github.com>
2024-02-17 01:52:35 +0200

    s

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -17,3 +17,17 @@
 	</h2>
 
 </section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 0.6;
+	}
+
+	h1 {
+		width: 100%;
+	}
+</style>
