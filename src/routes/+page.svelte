d77f3fc2a8c3a0779bc838e5a2948ceeabbd2c51
IviriusMain <106150547+IviriusMain@users.noreply.github.com>
2024-02-17 02:36:18 +0200

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,7 +13,7 @@
 		<Fluent.TextBlock variant="titleLarge">Ivirius</Fluent.TextBlock>
 	</h1>
  <h2>
-		<Fluent.TextBlock variant="subtitle">We made the #1 WordPad app in the Microsoft Store, which is available for Windows 10 and 11.</Fluent.TextBlock>
+		<Fluent.TextBlock variant="body">We made the #1 WordPad app in the Microsoft Store, which is available for Windows 10 and 11.</Fluent.TextBlock>
  </h2>
 </section>
 
