f3617ee03ddfe2c93b77386f59d96997b6a79a76
IviriusMain <106150547+IviriusMain@users.noreply.github.com>
2024-02-17 11:33:09 +0200

    Update +page.svelte

--- src/routes/ivirius-text-editor-plus/+page.svelte
+++ src/routes/ivirius-text-editor-plus/+page.svelte
@@ -1,1 +1,53 @@
-a
+<script>
+	import * as Fluent from "fluent-svelte";
+	import "fluent-svelte/theme.css";
+</script>
+
+<svelte:head>
+	<title>Ivirius</title>
+	<meta name="description" content="Ivirius official website" />
+</svelte:head>
+
+<section class="centered-section">
+	<h1>
+		<Fluent.TextBlock variant="titleLarge">Ivirius</Fluent.TextBlock>
+	</h1>
+ <h2>
+		<Fluent.TextBlock variant="body" align="center">We made the #1 WordPad app in the Microsoft Store, which is available for Windows 10 and 11.</Fluent.TextBlock>
+ </h2>
+</section>
+ 
+<section class="centered-section">
+	<h2>
+		<Fluent.Button variant="accent" onclick="window.location.href='https://apps.microsoft.com/detail/9N4T9H9182J5?hl=en-US';">Download Ivirius Text Editor Plus</Fluent.Button>
+		<Fluent.Button style="margin: 15px">Download Ivirius Text Editor</Fluent.Button>
+		<Fluent.Button>Download Ivirius Hub</Fluent.Button>
+	</h2>
+
+</section>
+ 
+<section class="margin-section">
+	<h1>
+		<Fluent.TextBlock variant="subtitle">Ivirius Text Editor Plus</Fluent.TextBlock>
+ </h1>
+ <h2>
+		<Fluent.TextBlock variant="body">Ivirius Text Editor Plus is a rich text editor app built on the Windows App SDK using WinUI 3.</Fluent.TextBlock>
+	</h2>
+
+</section>
+
+<style>
+	@import url("https://unpkg.com/fluent-svelte/theme.css");
+
+	/* Some base styles to get things looking right. */
+	:global(body) {
+		background-color: var(--fds-solid-background-base);
+		color: var(--fds-text-primary);
+	}
+    .centered-section {
+        text-align: center;
+    }
+    .margin-section {
+        margin: 25px;
+    }
+</style>
