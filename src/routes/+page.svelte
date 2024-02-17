542dcc2b375190da658871e355425de2363e7780
IviriusMain <106150547+IviriusMain@users.noreply.github.com>
2024-02-17 02:00:09 +0200

    s

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -20,6 +20,13 @@
 </section>
 
 <style>
+	@import url("https://unpkg.com/fluent-svelte/theme.css");
+
+	/* Some base styles to get things looking right. */
+	:global(body) {
+		background-color: var(--fds-solid-background-base);
+		color: var(--fds-text-primary);
+	}
 	section {
 		display: flex;
 		flex-direction: column;
