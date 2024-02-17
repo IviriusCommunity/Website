55e14f9016236ca122ef48bc95ae35f9ad1ab8aa
IviriusMain <106150547+IviriusMain@users.noreply.github.com>
2024-02-17 13:22:59 +0200

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,7 +8,7 @@
 	<meta name="description" content="Ivirius official website" />
 </svelte:head>
 
-<section style="display: flex; align-items: left; background: rgba(0, 0, 0, 0.15);">
+<section style="display: flex; flex-wrap: wrap; align-items: left; background: rgba(0, 0, 0, 0.15);">
   <Fluent.PersonPicture src="https://cdn.discordapp.com/attachments/1137161703000375339/1188990519418834994/Ivirius.png?ex=65dd2236&is=65caad36&hm=f5647520279749bf9811d7908e38f0586bedeef666f65ccfee30fb727a36471b" style="width: 35px; height: 35px; margin: 20px;" />
   <Fluent.TextBlock variant="bodyLarge" style="margin-top: 25px">Ivirius</Fluent.TextBlock>
   <Fluent.Button style="margin-top: 22px; margin-bottom: 22px; margin-left: 15px">Home</Fluent.Button>
