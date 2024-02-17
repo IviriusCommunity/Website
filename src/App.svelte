e022329a7783eef6d5142c377e8acec0bd9ab54c
IviriusMain <106150547+IviriusMain@users.noreply.github.com>
2024-02-17 02:01:55 +0200

    s

--- src/App.svelte
+++ src/App.svelte
@@ -1,3 +1,13 @@
 <script>
 	import "fluent-svelte/theme.css";
 </script>
+
+<style>
+	@import url("https://unpkg.com/fluent-svelte/theme.css");
+
+	/* Some base styles to get things looking right. */
+	:global(body) {
+		background-color: var(--fds-solid-background-base);
+		color: var(--fds-text-primary);
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -20,13 +20,6 @@
 </section>
 
 <style>
-	@import url("https://unpkg.com/fluent-svelte/theme.css");
-
-	/* Some base styles to get things looking right. */
-	:global(body) {
-		background-color: var(--fds-solid-background-base);
-		color: var(--fds-text-primary);
-	}
 	section {
 		display: flex;
 		flex-direction: column;
