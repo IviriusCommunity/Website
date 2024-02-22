4bcdadd475b83b5228a12ce770dca16f6a7a337c
IviriusMain <106150547+IviriusMain@users.noreply.github.com>
2024-02-22 13:08:13 +0200

    Update +page.svelte

--- src/routes/crimsonui/+page.svelte
+++ src/routes/crimsonui/+page.svelte
@@ -84,33 +84,37 @@
 </section>
 
 <style>
-        @import url("https://unpkg.com/fluent-svelte/theme.css");
+    @import url("https://unpkg.com/fluent-svelte/theme.css");
 
-        /* Some base styles to get things looking right. */
-        :global(body) {
-                background-color: var(--fds-solid-background-base);
-                color: var(--fds-text-primary);  
-	--fds-accent-light-3: 7, 100%, 71%;
-	--fds-accent-light-2: 7, 100%, 67%;
-	--fds-accent-light-1: 7, 100%, 63%;
-	--fds-accent-base: 7, 100%, 54%;
-	--fds-accent-dark-1: 7, 100%, 41%;
-	--fds-accent-dark-2: 7, 100%, 35%;
-	--fds-accent-dark-3: 7, 100%, 27%;
-        }
-        :root {
-	--fds-accent-light-3: 7, 100%, 71%;
-	--fds-accent-light-2: 7, 100%, 67%;
-	--fds-accent-light-1: 7, 100%, 63%;
-	--fds-accent-base: 7, 100%, 54%;
-	--fds-accent-dark-1: 7, 100%, 41%;
-	--fds-accent-dark-2: 7, 100%, 35%;
-	--fds-accent-dark-3: 7, 100%, 27%;
-}
+    /* Some base styles to get things looking right. */
+    :global(body) {
+        background-color: var(--fds-solid-background-base);
+        color: var(--fds-text-primary);  
+    }
+
+    --fds-accent-light-3: 7, 100%, 71%;
+    --fds-accent-light-2: 7, 100%, 67%;
+    --fds-accent-light-1: 7, 100%, 63%;
+    --fds-accent-base: 7, 100%, 54%;
+    --fds-accent-dark-1: 7, 100%, 41%;
+    --fds-accent-dark-2: 7, 100%, 35%;
+    --fds-accent-dark-3: 7, 100%, 27%;
+
+    :root {
+        --fds-accent-light-3: 7, 100%, 71%;
+        --fds-accent-light-2: 7, 100%, 67%;
+        --fds-accent-light-1: 7, 100%, 63%;
+        --fds-accent-base: 7, 100%, 54%;
+        --fds-accent-dark-1: 7, 100%, 41%;
+        --fds-accent-dark-2: 7, 100%, 35%;
+        --fds-accent-dark-3: 7, 100%, 27%;
+    }
+
     .centered-section {
         text-align: center;
         margin: 25px;
     }
+
     .margin-section {
         margin: 25px;
     }
