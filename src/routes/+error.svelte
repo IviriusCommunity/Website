d4487656cae7922dc74de2e3e58c96231e537df5
IviriusMain <106150547+IviriusMain@users.noreply.github.com>
2024-02-22 02:06:26 +0200

    Update routes

---
+++ src/routes/+error.svelte
@@ -0,0 +1,54 @@
+<script>
+        import * as Fluent from "fluent-svelte";
+        import "fluent-svelte/theme.css";
+</script>
+
+<svelte:head>
+        <title>Ivirius</title>
+        <meta name="description" content="Ivirius official website" />
+</svelte:head>
+
+<section style="display: flex; flex-wrap: wrap; align-items: center; justify-content: flex-start; padding: 10px; background: rgba(25, 25, 25, 0.15); border-bottom: 1px solid rgba(205, 205, 205, 0.25);">
+  <Fluent.PersonPicture src="https://cdn.discordapp.com/attachments/1137161703000375339/1188990519418834994/Ivirius.png?ex=65dd2236&is=65caad36&hm=f5647520279749bf9811d7908e38f0586bedeef666f65ccfee30fb727a36471b" style="width: 35px; height: 35px; margin-right: 15px; margin-left: 15px; margin-top: 18px; align-items: center;"/>
+  <Fluent.TextBlock variant="bodyLarge" style="margin-right: 25px; align-items: center;">Ivirius</Fluent.TextBlock>
+  <Fluent.Button style="height: 32px; margin-right: 15px; align-items: center;" variant="hyperlink" onclick="window.location.href='https://ivirius.vercel.app';">Home</Fluent.Button>
+  <Fluent.Button style="height: 32px; margin-right: 15px; align-items: center;" variant="hyperlink" onclick="window.location.href='https://ivirius.vercel.app/crimsonui';">CrimsonUI</Fluent.Button>
+  <Fluent.Button style="height: 32px; margin-right: 15px; align-items: center;" variant="hyperlink" onclick="window.location.href='https://ivirius.vercel.app/documentations';">Documentations</Fluent.Button>
+  <Fluent.Button style="height: 32px; margin-right: 15px; align-items: center;" variant="hyperlink" onclick="window.location.href='https://ivirius.vercel.app/contact';">Contact us</Fluent.Button>
+  <Fluent.Button style="height: 32px; margin-right: 15px; align-items: center;" variant="hyperlink" onclick="window.location.href='https://ivirius.vercel.app/about';">About</Fluent.Button>
+</section>
+
+<section class="centered-section">
+        <h1>
+                <Fluent.TextBlock variant="titleLarge">Uh oh...</Fluent.TextBlock>
+        </h1>
+ <h2>
+                <Fluent.TextBlock variant="body" align="center">No one's here. Try checking the URL and try again.</Fluent.TextBlock>
+ </h2>
+</section>
+
+<section class="centered-section">
+        <h2>
+                <Fluent.Button variant="accent" onclick="window.location.href='https://ivirius.webnode.page';">Home page</Fluent.Button>
+        </h2>
+
+</section>
+
+<style>
+        @import url("https://unpkg.com/fluent-svelte/theme.css");
+
+        /* Some base styles to get things looking right. */
+        :global(body) {
+                background-color: var(--fds-solid-background-base);
+                color: var(--fds-text-primary);
+        }
+    .centered-section {
+        text-align: center;
+        margin: 25px;
+        max-width: 1800px;
+    }
+    .margin-section {
+        margin: 25px;
+        max-width: 1800px;
+    }  
+</style>
