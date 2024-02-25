19f5e0c1f67f2a4f07067388bbce65d15c142ce1
IviriusMain <106150547+IviriusMain@users.noreply.github.com>
2024-02-25 11:17:41 +0000

    Updated contact

---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,125 @@
+<script>
+  //Imports
+	import * as Fluent from "fluent-svelte";
+	import "fluent-svelte/theme.css";
+
+  //Variables
+  let open = true;
+</script>
+
+<!--Head-->
+<svelte:head>
+	<title>Ivirius</title>
+	<meta name="description" content="Ivirius official website"/>
+</svelte:head>
+
+<!--Navbar-->
+<section style="display: flex; flex-wrap: wrap; align-items: center; justify-content: flex-start; padding: 10px; background: var(--fds-solid-background-base); border-bottom: 1px solid rgba(205, 205, 205, 0.25);">
+
+  <!--Favicon-->
+  <Fluent.PersonPicture src="https://cdn.discordapp.com/attachments/1137161703000375339/1188990519418834994/Ivirius.png?ex=65dd2236&is=65caad36&hm=f5647520279749bf9811d7908e38f0586bedeef666f65ccfee30fb727a36471b" style="width: 35px; height: 35px; margin-right: 15px; margin-left: 15px; margin-top: 18px; align-items: center;"/>
+
+  <!--Title-->
+  <Fluent.TextBlock variant="bodyLarge" style="margin-right: 25px; align-items: center;">Ivirius</Fluent.TextBlock>
+
+  <!--Home-->
+  <Fluent.Button style="height: 32px; margin-right: 15px; align-items: center;" variant="hyperlink" onclick="window.location.href='https://ivirius.vercel.app';">Home</Fluent.Button>
+  
+  <!--CrimsonUI-->
+  <Fluent.Button style="height: 32px; margin-right: 15px; align-items: center;" variant="hyperlink" onclick="window.location.href='https://ivirius.vercel.app/crimsonui';">CrimsonUI</Fluent.Button>
+
+  <!--Documentations-->
+  <Fluent.Button style="height: 32px; margin-right: 15px; align-items: center;" variant="hyperlink" onclick="window.location.href='https://ivirius.vercel.app/documentations';">Documentations</Fluent.Button>
+
+  <!--Contact us-->
+  <Fluent.Button style="height: 32px; margin-right: 15px; align-items: center;">Contact us</Fluent.Button>
+
+  <!--About-->
+  <Fluent.Button style="height: 32px; margin-right: 15px; align-items: center;" variant="hyperlink" onclick="window.location.href='https://ivirius.vercel.app/about';">About</Fluent.Button>
+</section>
+
+<!--Title-->
+<section class="centered-section">
+	<h1>
+		<Fluent.TextBlock variant="titleLarge">Contact us</Fluent.TextBlock>
+	</h1>
+</section>
+
+ <section class="margin-section">
+	<h2>
+		<Fluent.InfoBar severity="caution" title="Important!" message="This page is not functional yet." style="max-width:550px;"/>
+	</h2>
+</section>
+
+<!--Ivirius Text Editor Plus-->
+<section class="margin-section">
+        <h1>
+                <Fluent.TextBox placeholder="Email" type="email"></Fluent.TextBox>
+        </h1>
+        <h2>
+                <Fluent.TextBox placeholder="Subject"></Fluent.TextBox>
+        </h2>
+        <h3>
+          <Fluent.TextBox placeholder="Message" style="min-height: 150px;"></Fluent.TextBox>
+        </h3>
+</section>
+
+<section class="margin-section">
+<h1>
+  
+</h1>
+</section>
+
+<!--Bottom bar-->
+<section style="padding-top: 10px; padding-bottom: 10px; padding-left: 25px; background: var(--fds-solid-background-base); border-top: 1px solid rgba(205, 205, 205, 0.25); display: flex; flex-direction: column; align-items: flex-start;">
+  <Fluent.TextBlock variant="bodyStrong" style="margin-top: 10px;">
+    Partners
+  </Fluent.TextBlock>
+          <Fluent.Button variant="hyperlink" onclick="window.location.href='https://www.spoo.me';" style="margin-top: 10px; margin-bottom: 10px;">spoo.me URL shortener</Fluent.Button>
+  <Fluent.TextBlock variant="bodyStrong" style="margin-top: 10px;">
+    Developers and contributors
+  </Fluent.TextBlock>
+          <Fluent.Button variant="hyperlink" onclick="window.location.href='https://website-2-sigma.vercel.app/';" style="margin-top: 10px; margin-bottom: 10px;">ErrorTek</Fluent.Button>
+  <Fluent.TextBlock variant="bodyStrong" style="margin-top: 10px;">
+    Website
+  </Fluent.TextBlock>
+          <Fluent.Button variant="hyperlink" onclick="window.location.href='https://fluent-svelte.vercel.app';" style="margin-top: 10px; margin-bottom: 10px;">Fluent Svelte</Fluent.Button>
+          <Fluent.Button variant="hyperlink" onclick="window.location.href='https://www.vercel.com';">Vercel</Fluent.Button>
+          <Fluent.Button variant="hyperlink" onclick="window.location.href='https://ivirius.vercel.app/about';" style="margin-top: 10px; margin-bottom: 10px;">About</Fluent.Button>
+</section>
+
+<!--Styles-->
+<style>
+  /*Import theme*/
+	@import url("https://unpkg.com/fluent-svelte/theme.css");
+
+	/* Some base styles to get things looking right. */
+	:global(body) 
+  {
+    /*Background color*/
+		background-color: var(--fds-solid-background-base);
+
+    /*Background image*/
+    background-image: url("https://cdn.discordapp.com/attachments/1141503151808184401/1210350581504278618/1000030651-safeimagekit.jpeg.png?ex=65ea3dd1&is=65d7c8d1&hm=facb30449de806bcb7bb777bd14e81dddb7bf7de9e882144896e0bc80b304153&");
+    
+    /*Background color*/
+		color: var(--fds-text-primary);
+	}
+
+  /*Centered section*/
+  .centered-section 
+  {
+    text-align: center;
+    margin: 0 auto;
+    padding: 25px;
+    max-width: 850px;
+  }
+
+  /*Left aligned centered section*/
+  .margin-section 
+  {
+    margin: 0 auto;
+    padding: 25px;
+    max-width: 850px;
+  }
+  </style>
