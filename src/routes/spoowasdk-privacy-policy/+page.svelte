49200960953da5b7476c16da144f81eeb12e0859
IviriusMain <106150547+IviriusMain@users.noreply.github.com>
2024-02-27 23:27:48 +0000

    Fixed text

--- src/routes/spoowasdk-privacy-policy/+page.svelte
+++ src/routes/spoowasdk-privacy-policy/+page.svelte
@@ -72,15 +72,26 @@
                 <Fluent.TextBlock variant="subtitle">Data Collection</Fluent.TextBlock>
         </h1>
         <h2>
-                <Fluent.TextBlock variant="body">spooWASDK utilizes HTTP requests to shorten URLs through the spoo.me API. To ensure the functionality of the maximum requests cap amd URL shortening, the following data is collected:
-
-1. Data inputted during application usage.
-2. Computer's IP address.
-
-This limited data collection is solely for the purpose of ensuring the base functionality and adhering to the maximum requests cap.
-
-We value and prioritize your privacy. This privacy policy outlines our commitment to maintaining the confidentiality and security of the data collected during the use of spooWASDK. </Fluent.TextBlock>
+                <Fluent.TextBlock variant="body">spooWASDK utilizes HTTP requests to shorten URLs through the spoo.me API. To ensure the functionality of the maximum requests cap amd URL shortening, the following data is collected:</Fluent.TextBlock>
         </h2>
+<h3>
+          <Fluent.TextBlock variant="body">
+1. Data inputted during application usage.</Fluent.TextBlock>
+</h3>
+<h4>
+          <Fluent.TextBlock variant="body">
+2. Computer's IP address.
+</Fluent.TextBlock>
+</h4>
+<h5>
+          <Fluent.TextBlock variant="body">
+This limited data collection is solely for the purpose of ensuring the base functionality and adhering to the maximum requests cap.
+            </Fluent.TextBlock>
+</h5>
+<h6>
+                  <Fluent.TextBlock variant="body">
+We value and prioritize your privacy. This privacy policy outlines our commitment to maintaining the confidentiality and security of the data collected during the use of spooWASDK. </Fluent.TextBlock>
+        </h6>
 </section>
 
 <!--Bottom bar-->
