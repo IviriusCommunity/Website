8caa023f0152fa1cd9bc1f4c8c0ed0d251c0a967
IviriusMain <106150547+IviriusMain@users.noreply.github.com>
2024-03-15 00:45:35 +0000

    Fixed hook

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -6,7 +6,9 @@
   //Variables
   let open = true;
 
-    let email, subject, message;
+  let email = "";
+  let subject = "";
+  let message = "";
 
   async function sendToDiscord() {
     const webhookUrl = 'https://discord.com/api/webhooks/1217994733788598393/N7ctffAAkA0ejbMcyaCjjQWBCb1ISurlmaalys0_hBAl2c9ngl5gldWXGdEpZ3r_X6HG'; // Replace with your Discord webhook URL
