1a7f3afaebbf2de25283e4ba269925803c906787
jpb <jakub.baranski.33@gmail.com>
2024-07-13 17:56:09 +0100

    Use the same navbar on every page

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -83,53 +83,28 @@
 </svelte:head>
 
 <!--Navbar-->
-<section
-	style="display: flex; flex-wrap: wrap; align-items: center; justify-content: flex-start; padding: 10px; background: var(--fds-solid-background-base); border-bottom: 1px solid rgba(205, 205, 205, 0.25);"
->
+<section style="display: flex; flex-wrap: wrap; align-items: center; justify-content: flex-start; padding: 10px; background: var(--fds-solid-background-base); border-bottom: 1px solid rgba(205, 205, 205, 0.25);">
+
 	<!--Favicon-->
-	<Fluent.PersonPicture
-		src="/favicon.png"
-		style="width: 35px; height: 35px; margin-right: 15px; margin-left: 15px; margin-top: 18px; align-items: center;"
-	/>
+	<Fluent.PersonPicture src="/favicon.png" style="width: 35px; height: 35px; margin-right: 15px; margin-left: 15px; margin-top: 18px; align-items: center;"/>
 
 	<!--Title-->
-	<Fluent.TextBlock variant="bodyLarge" style="margin-right: 25px; align-items: center;"
-		>Ivirius</Fluent.TextBlock
-	>
+	<Fluent.TextBlock variant="bodyLarge" style="margin-right: 25px; align-items: center;">Ivirius</Fluent.TextBlock>
 
 	<!--Home-->
-	<Fluent.Button
-		style="height: 32px; margin-right: 15px; align-items: center;"
-		variant="hyperlink"
-		onclick="window.location.href='/';">Home</Fluent.Button
-	>
+	<Fluent.Button style="height: 32px; color: var(--fds-accent-text-primary); margin-right: 15px; align-items: center;">Home</Fluent.Button>
 
 	<!--CrimsonUI-->
-	<Fluent.Button
-		style="height: 32px; margin-right: 15px; align-items: center;"
-		variant="hyperlink"
-		onclick="window.location.href='/crimsonui';">CrimsonUI</Fluent.Button
-	>
+	<Fluent.Button style="color: var(--fds-text-primary); height: 32px; margin-right: 15px; align-items: center;" variant="hyperlink" onclick="window.location.href='/crimsonui';">CrimsonUI</Fluent.Button>
 
 	<!--Documentations-->
-	<Fluent.Button
-		style="height: 32px; margin-right: 15px; align-items: center;"
-		variant="hyperlink"
-		onclick="window.location.href='/documentations';"
-		>Documentations</Fluent.Button
-	>
+	<Fluent.Button style="color: var(--fds-text-primary); height: 32px; margin-right: 15px; align-items: center;" variant="hyperlink" onclick="window.location.href='/documentations';">Documentations</Fluent.Button>
 
 	<!--Contact us-->
-	<Fluent.Button style="height: 32px; margin-right: 15px; align-items: center;"
-		>Contact us</Fluent.Button
-	>
+	<Fluent.Button style="color: var(--fds-text-primary); border-color: var(--fds-accent-text-primary); height: 32px; margin-right: 15px; align-items: center;" variant="hyperlink" onclick="window.location.href='/contact';">Contact us</Fluent.Button>
 
 	<!--About-->
-	<Fluent.Button
-		style="height: 32px; margin-right: 15px; align-items: center;"
-		variant="hyperlink"
-		onclick="window.location.href='/about';">About</Fluent.Button
-	>
+	<Fluent.Button style="color: var(--fds-text-primary); height: 32px; margin-right: 15px; align-items: center;" variant="hyperlink" onclick="window.location.href='/about';">About</Fluent.Button>
 </section>
 
 <!--Title-->
